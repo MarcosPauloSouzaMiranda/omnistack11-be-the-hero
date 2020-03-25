0f0893588360ef784231c8e1d5f37e0fa7dd7d4b
Marcos Paulo Souza Miranda <marcospsmiranda@gmail.com>
2020-03-25 12:21:40 -0300

    Implementação do componente header com props.

--- frontend/web/src/App.js
+++ frontend/web/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import Header from './Header';
+
 function App() {
   return (
-    <div className="App">
-     Aplicação
-    </div>
+    <Header>
+      Semana Omnistack
+    </Header>
   );
 }
 
