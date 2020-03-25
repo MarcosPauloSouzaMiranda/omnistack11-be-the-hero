b71ff18e44697bc3f526bdb2c33983035d309272
Marcos Paulo Souza Miranda <marcospsmiranda@gmail.com>
2020-03-25 12:27:10 -0300

    Implementação do estado incremental!

--- frontend/web/src/App.js
+++ frontend/web/src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './Header';
 
 function App() {
+
+  const [counter, setCounter] = useState(0);
+
+  function increment(){
+    setCounter(counter + 1);
+  }
+
   return (
     <Header>
-      Semana Omnistack
+      Semana Omnistack <br />
+      <strong>Counter: </strong> { counter } <br />
+      <button onClick={increment}>Adicionar</button>
     </Header>
   );
 }
