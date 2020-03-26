51e0e43c7f2700c129d6b31d928358a26e27325f
Marcos Paulo Souza Miranda <marcospsmiranda@gmail.com>
2020-03-25 21:20:11 -0300

    Criação do componente register e implementação do router dom.

--- frontend/web/src/pages/Logon/index.js
+++ frontend/web/src/pages/Logon/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiLogIn } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 import './style.css';
 
@@ -18,10 +19,10 @@ export default function Logon() {
           <input type="text" placeholder="Sua ID"/>
           <button className="button" type="submit">Entrar</button>
 
-          <a href="/register">
+          <Link to="/register">
             <FiLogIn color="#E02041" size={16} />
             Não tenho cadastro
-          </a>
+          </Link>
         </form>
       </section>
 
